0cceaa5f1d7b0670539ca5a8eea1422d476e203e
Ibrahim Khalil Shakik <shakik.social@gmail.com>
2022-12-05 00:18:04 +0600

    Boxing Unboxing

---
+++ CSharpInterviewPrep/CSharpInterviewPrep/Models/BoxingUnboxing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterviewPrep.Models
+{
+    internal class BoxingUnboxing
+    {
+        private static int i = 10;
+        private static object obj = i; // boxing
+        private int j = (int)obj; // unboxing
+    }
+}
